24c96fd369635baf2acdd1343a34dce67352f2bd
HyeJun <solar3070@naver.com>
2021-05-05 16:57:49 +0900

    #11-Stocks

---
+++ app/src/main/java/ddwucom/mobile/healthstock/dto/Stocks.java
@@ -0,0 +1,10 @@
+package ddwucom.mobile.healthstock.dto;
+
+import java.util.Date;
+
+public class Stocks {
+    private int stocksId;
+    private int sharePrice;
+    private Date date;
+    private UserInfo userName;
+}
