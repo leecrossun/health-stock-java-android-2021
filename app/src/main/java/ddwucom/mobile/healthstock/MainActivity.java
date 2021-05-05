de36986604ae8baf282c91cc059121965f9e6500
HyeJun <solar3070@naver.com>
2021-05-05 19:12:44 +0900

    stocks layout

--- app/src/main/java/ddwucom/mobile/healthstock/MainActivity.java
+++ app/src/main/java/ddwucom/mobile/healthstock/MainActivity.java
@@ -1,6 +1,13 @@
 package ddwucom.mobile.healthstock;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,7 +17,11 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,32 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        //jiwoo hello~~
+        chart = findViewById(R.id.linechart);
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+
+            float val = (float) (Math.random() * 10);
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+        set1 = new LineDataSet(values, "DataSet 1");
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1); // add the data sets
+
+        // create a data object with the data sets
+        LineData data = new LineData(dataSets);
+
+        // black lines and points
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+
+        // set data
+        chart.setData(data);
     }
+
 
 }
