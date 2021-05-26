6b4651ebb7944deff9f63291b7f4b6db9be20e0a
janu01ary <janu01ary@gmail.com>
2021-05-26 15:26:35 +0900

    link position acitivity ready

--- app/src/main/java/ddwucom/mobile/healthstock/PositionActivity.java
+++ app/src/main/java/ddwucom/mobile/healthstock/PositionActivity.java
@@ -2,12 +2,15 @@ package ddwucom.mobile.healthstock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class PositionActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,9 @@ public class PositionActivity extends AppCompatActivity {
             Toast.makeText(this, "측정 시작", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "측정 종료", Toast.LENGTH_SHORT).show();
+//            intent = new Intent(this, .class);
+            startActivity(intent);
+            finish();
         }
     }
 
