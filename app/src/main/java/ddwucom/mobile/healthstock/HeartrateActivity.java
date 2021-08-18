d5a27f49fb0a39c9da73dfe159e649d2f5edd88c
hyosunLee <leecrossun@naver.com>
2021-08-18 11:00:43 +0900

    [48]heartRate

--- app/src/main/java/ddwucom/mobile/healthstock/HeartrateActivity.java
+++ app/src/main/java/ddwucom/mobile/healthstock/HeartrateActivity.java
@@ -12,13 +12,27 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HeartrateActivity extends AppCompatActivity {
     boolean run_init = true;
     Thread raspberryThread;
     boolean run_rasp = true;
+    TextView heartRate;
+    Socket socket;
+    Handler handler;
+
 
 
     @Override
@@ -26,12 +40,17 @@ public class HeartrateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heartrate);
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        heartRate = findViewById(R.id.txt_heartrate);
+        handler = new Handler();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_start:
+                HeartrateActivity.ServerThread thread = new HeartrateActivity.ServerThread();
+                Log.d("test", "onclick is on");
+                thread.start();
                 Toast.makeText(HeartrateActivity.this, "심박수 측정을 시작합니다.", Toast.LENGTH_SHORT).show();
 
                 break;
@@ -53,7 +72,66 @@ public class HeartrateActivity extends AppCompatActivity {
                     }
                 });
                 dlg.show();
+                finish();
                 break;
+        }
+    }
+
+    class ServerThread extends Thread {
+        @Override
+        public void run() {
+            int port = 9999;
+
+            try {
+                ServerSocket server = new ServerSocket(port);
+                Log.d("ServerThread", "Server Started.");
+
+                while(true){
+                    socket = server.accept();
+
+                    InputStream input = socket.getInputStream();
+                    if (socket.isConnected()){
+                        Log.d("socket", "connect");
+                        Log.d("socket", String.valueOf(input.available()));
+                    }
+
+                    byte[] byteArr = new byte[100];
+                    int readByteCount = input.read(byteArr);
+                    String data = new String(byteArr, 0, readByteCount, "UTF-8");
+                    //결과값 띄워주기
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    heartRate.setText(data);// Todo: 여기에 UI 작업할 코드를 입력하시면 됩니다.
+                                }
+                            });
+                        }
+                    }).start();
+                    Log.d("socket", "input: " + data);
+
+                    OutputStream out = socket.getOutputStream();
+                    PrintWriter writer = new PrintWriter(out, true);
+                    writer.println("server send");
+                    Log.d("socket", "output: " + data);
+                    socket.close();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            socket.close();//종료시 소켓도 닫아주어야한다.
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
